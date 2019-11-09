3970a4d36d8131a22e0fc4a25e4f22c43a4e09d2
Orlan Quijada <orlantquijada@gmail.com>
2019-11-09 13:05:59 +0800

    forms

--- src/Pages/form.jsx
+++ src/Pages/form.jsx
@@ -1,41 +1,54 @@
-import React, { Component } from 'react';
-import Navbar from '../components/Navbar';
-import MyForm from './form1';
-import './pages css/forms.css';
+import React, { Component } from "react";
+import Navbar from "../components/Navbar";
+import MyForm from "./form1";
+import "./pages css/forms.css";
 class Forms extends Component {
-	state = {};
-	render() {
-		return (
-			<React.Fragment>
-				<Navbar />
-				<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
-				<main style={{ marginTop: 90 }}>
-					<div style={{ textAlign: 'center', marginBottom: 30 }}>
-						<h1>Report a Violation</h1>
-					</div>
-					<hr style={{ borderTop: '3px solid rgba(0, 0, 0, 0.1)' }} />
-					<div style={{ textAlign: 'center', padding: '0px 15%' }}>
-						<div
-							style={{
-								marginTop: 50,
-								border: 'solid 1.4px',
-								borderColor: 'darkgrey'
-							}}
-							class="card"
-						>
-							<h5
-								style={{ backgroundColor: 'skyblue', color: 'white', fontSize: 35 }}
-								class="card-header info-color white-text text-center py-4"
-							>
-								<strong>Report Form</strong>
-							</h5>
-						</div>
-						<MyForm />
-					</div>
-				</main>
-			</React.Fragment>
-		);
-	}
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+          rel="stylesheet"
+        />
+        <main style={{}}>
+          <div style={{ textAlign: "center", marginBottom: 10, marginTop: 10 }}>
+            <h1>Report a Violation</h1>
+          </div>
+          <hr style={{ borderTop: "3px solid rgba(0, 0, 0, 0.1)" }} />
+          <div
+            style={{
+              textAlign: "center",
+              margin: "0px 32%",
+              textAlign: "center",
+
+              border: "solid"
+            }}
+          >
+            <div
+              style={{
+                border: "solid 1.4px",
+                borderColor: "darkgrey"
+              }}
+              class="card"
+            >
+              <h5
+                style={{
+                  backgroundColor: "skyblue",
+                  color: "white",
+                  fontSize: 35
+                }}
+                class="card-header info-color white-text text-center py-4"
+              >
+                <strong>Report Form</strong>
+              </h5>
+            </div>
+            <MyForm />
+          </div>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Forms;
--- src/components/PaymentCards.jsx
+++ src/components/PaymentCards.jsx
@@ -1,128 +1,131 @@
-import React, { Component, useState } from 'react';
-import Pagination from './Pagination';
-import PModal from './PModal.jsx';
-import axios from 'axios';
-import Pcards from './Pcards';
+import React, { Component, useState } from "react";
+import Pagination from "./Pagination";
+import PModal from "./PModal.jsx";
+import axios from "axios";
+import Pcards from "./Pcards";
 
 const data = [
-	{
-		violation: 'Double Parking',
-		description:
-			'YOU DOUBLE PARKED! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.',
-		deadline: 'October 23, 2019 ',
-		fine: 10000
-	}
+  {
+    violation: "Double Parking",
+    description:
+      "YOU DOUBLE PARKED! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
+    deadline: "October 23, 2019 ",
+    fine: 10000
+  }
 ];
 class PaymentCards extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			modalPShow: false,
-			payments: []
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalPShow: false,
+      payments: []
+    };
+  }
 
-	componentDidMount() {
-		this.refresh();
-	}
+  componentDidMount() {
+    this.refresh();
+  }
 
-	refresh = () => {
-		axios
-			.get('/v1/fees/')
-			.then((res) => this.setState({ payments: res.data.results }))
-			.catch((err) => console.log(err));
-	};
+  refresh = () => {
+    axios
+      .get("/v1/fees/")
+      .then(res => this.setState({ payments: res.data.results }))
+      .catch(err => console.log(err));
+  };
 
-	renderPayments = () => {
-		const newPay = this.state.payments;
-		console.log(this.state.payments);
-		return newPay.map((item, i) => (
-			<div
-				style={{
-					marginBottom: 20,
-					marginLeft: 10,
-					borderRadius: 10,
-					marginRight: 10
-				}}
-				className="card"
-				key={i}
-			>
-				<div style={{ fontSize: 30, backgroundColor: '#90c3fb' }} className="card-header card-primary">
-					{' '}
-					<span style={{ marginRight: 10 }}>
-						<b>{item.violation}</b>
-					</span>
-				</div>
-				<div className="card-body">
-					<blockquote
-						style={{
-							fontSize: 30,
-							borderLeft: 'solid',
-							borderColor: '#3dcc4f',
-							marginBottom: 10
-						}}
-						className="blockquote mb-0"
-					>
-						<p>{item.description}</p>
-					</blockquote>
-					<div style={{ fontSize: 30, marginBottom: 0, marginTop: 10 }}>
-						Deadline: {item.deadline}
-						<span style={{ float: 'right' }}>PHP {item.fine}</span>
-					</div>
-					<div style={{ fontSize: 30, float: 'right' }}>
-						<button
-							style={{ backgroundColor: '#90c3fb', fontSize: 30 }}
-							type="button"
-							className="btn"
-							onClick={() => this.setState({ modalPShow: true })}
-						>
-							{/* <PayPalButton
+  renderPayments = () => {
+    const newPay = this.state.payments;
+    console.log(this.state.payments);
+    return newPay.map((item, i) => (
+      <div
+        style={{
+          marginBottom: 20,
+          marginLeft: 10,
+          borderRadius: 10,
+          marginRight: 10
+        }}
+        className="card"
+        key={i}
+      >
+        <div
+          style={{ fontSize: 30, backgroundColor: "#90c3fb" }}
+          className="card-header card-primary"
+        >
+          {" "}
+          <span style={{ marginRight: 10 }}>
+            <b>{item.violation}</b>
+          </span>
+        </div>
+        <div className="card-body">
+          <blockquote
+            style={{
+              fontSize: 30,
+              borderLeft: "solid",
+              borderColor: "#3dcc4f",
+              marginBottom: 10
+            }}
+            className="blockquote mb-0"
+          >
+            <p>{item.description}</p>
+          </blockquote>
+          <div style={{ fontSize: 30, marginBottom: 0, marginTop: 10 }}>
+            Deadline: {item.deadline}
+            <span style={{ float: "right" }}>PHP {item.fine}</span>
+          </div>
+          <div style={{ fontSize: 30, float: "right" }}>
+            <button
+              style={{ backgroundColor: "#90c3fb", fontSize: 30 }}
+              type="button"
+              className="btn"
+              onClick={() => this.setState({ modalPShow: true })}
+            >
+              {/* <PayPalButton
 								// currency="PHP"
 								amount={item.fine}
 								onSuccess={(details, data) => {
 									alert('Transaction completed by ' + details.payer.name.given_name);
 								}}
 							/> */}
-							Pay
-						</button>
+              Pay
+            </button>
 
-						<PModal
-							fine={item.fine}
-							show={this.state.modalPShow}
-							onHide={() => this.setState({ modalPShow: false })}
-						/>
-					</div>
-				</div>
-			</div>
-		));
-	};
+            <PModal
+              fine={item.fine}
+              show={this.state.modalPShow}
+              onHide={() => this.setState({ modalPShow: false })}
+            />
+          </div>
+        </div>
+      </div>
+    ));
+  };
 
-	render() {
-		return (
-			<div
-				style={{
-					marginTop: 30
-				}}
-			>
-				<hr />
-				<div style={{ height: 800 }}>
-					{/* <Pcards />
+  render() {
+    return (
+      <div
+        style={{
+          marginTop: 30
+        }}
+      >
+        <hr />
+        <div style={{ height: 800 }}>
+          {/* <Pcards />
           <Pcards />
 		  <Pcards /> */}
-					{this.renderPayments()}
-					<div
-						style={{
-							position: 'absolute',
-							bottom: '-43%',
-							right: '43%'
-						}}
-					>
-						<Pagination />
-					</div>
-				</div>
-			</div>
-		);
-	}
+          {this.renderPayments()}
+          <div
+            style={{
+              position: "absolute",
+              bottom: "-43%",
+              right: "43%"
+            }}
+          >
+            {/* <Pagination /> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default PaymentCards;
